7bb48f888c1c736f75b63e20e8dbf8446eb05bd7
sami-aburwaise <tarrary1@gmail.com>
2023-09-07 11:07:24 +0300

    play button function, buttons color

--- scripts/page_script.js
+++ scripts/page_script.js
@@ -1,9 +1,19 @@
 const playButton = document.querySelector('#button')
 
+const colorButtons = document.querySelectorAll('button')
+
 playButton.addEventListener('mouseover', () => {
   playButton.style.backgroundColor = 'rgb(45, 45, 45)'
 })
 
 playButton.addEventListener('mouseout', () => {
   playButton.style.backgroundColor = 'black'
+})
+
+playButton.addEventListener('click', () => {
+  window.location.href = 'game.html'
+})
+
+colorButtons.forEach((button) => {
+  button.style.backgroundColor = button.innerText
 })
